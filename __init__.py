aac40adba37175bd1e3d837b2bfcbed32e5ec5c1
Kilb, Philipp <philipp.kilb@outlook.de>
2020-02-06 11:46:35 +0100

    copy paste

--- __init__.py
+++ __init__.py
@@ -17,7 +17,9 @@
 
 from adapt.intent import IntentBuilder
 from mycroft import MycroftSkill, intent_handler
-import addon
+import os
+import json
+#import addon
 
 class HelloWorldSkill(MycroftSkill):
     def __init__(self):
@@ -63,6 +65,34 @@ class HelloWorldSkill(MycroftSkill):
     def convese(self, utterance, lang="en-us"):
         self.user_input = utterance
 
+
+
+
+
+
+class questionnaire:
+    def __init__(self, skill_triggered, utterance):
+        self.skill = skill_triggered
+        self.utterance = utterance
+        self.survey = []
+
+    def ask_and_save(self, number):
+        question = self.get_question(number)
+        answer = self.skill.ask_yesno(question)
+        self.survey.append((self.utterance[0], question, answer))
+
+    def skill_interaction_response(self):
+        self.ask_and_save(1)
+        self.ask_and_save(2)
+        # self.speak_dialog(str(self.survey))
+        survey_copy = self.survey.copy()
+        with open(os.path.join(self.skill.root_dir, 'log_file_ours.json'), 'w') as f:
+            json.dump(survey_copy, f, indent=4, sort_keys=True)
+
+    def get_question(self, number):
+        question = {1: "Do you know you lost private information?",
+                    2: "In your opinion which information got lost?"}
+        return question[number]
 
 
 
