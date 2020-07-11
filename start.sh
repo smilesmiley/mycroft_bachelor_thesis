9b4932dd40df3432be5bec88c0284baed19cb3a8
smilesmiley <s8aagrie@stud.uni-saarland.de>
2020-07-11 17:45:01 +0200

    readjustet skript

--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@ rm mic.py
 
 cd ../../../
 
-cp skills/mycroft_bachelor_thesis.smilesmiley/setup_one/mycroft_skill.py mycroft/skills/mycroft_skill/
+cp skills/mycroft_bachelor_thesis.smilesmiley/mycroft_skill.py mycroft/skills/mycroft_skill/
 cp skills/mycroft_bachelor_thesis.smilesmiley/mic.py mycroft/client/speech/
 cp skills/mycroft_bachelor_thesis.smilesmiley/listener.py mycroft/client/speech/
 bash start-mycroft.sh all
