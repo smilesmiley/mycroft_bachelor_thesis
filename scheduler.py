58f41f38f4663cb525379d3940d7cab6b76406c3
smilesmiley <s8aagrie@stud.uni-saarland.de>
2020-08-01 14:47:51 +0200

    a

--- scheduler.py
+++ scheduler.py
@@ -11,13 +11,13 @@ def job_copy():
     # to do do only once the job
     return schedule.CancelJob
 
-def job_script():
-    subprocess.call('./start.sh')
+def job_general():
+    subprocess
     return schedule.CancelJob
 # schedule.every(10).minutes.do(job)
 # schedule.every().hour.do(job)
 # schedule.every().day.at("10:30").do(job)
-schedule.every().day.at("13:23").do(job_copy)
+schedule.every().day.at("13:49").do(job_copy)
 # schedule.every().day.at("12:56").do(job_script)
 
 while 1:
