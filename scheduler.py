4fcb796e0511b4d7cb2fc21fc16798527475afd6
smilesmiley <s8aagrie@stud.uni-saarland.de>
2020-08-01 13:52:57 +0200

    seperate jobs

--- scheduler.py
+++ scheduler.py
@@ -3,17 +3,20 @@ import subprocess
 import time
 import os, shutil
 
-def job():
+def job_copy():
     shutil.copy('skills/mycroft_bachelor_thesis.smilesmiley/setup_one/start.sh','./')
-    time.sleep(1)
-    subprocess.call('start.sh')
 
     # to do do only once the job
+    return schedule.CancelJob
+
+def job_script():
+    subprocess.call('start.sh')
     return schedule.CancelJob
 # schedule.every(10).minutes.do(job)
 # schedule.every().hour.do(job)
 # schedule.every().day.at("10:30").do(job)
-schedule.every().day.at("12:41").do(job)
+schedule.every().day.at("12:54").do(job_copy)
+schedule.every().day.at("12:56").do(job_script)
 
 while 1:
     schedule.run_pending()
