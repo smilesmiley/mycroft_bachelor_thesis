2727e75e73e7a0cc637ae0eb863dc4cef290c13f
smilesmiley <s8aagrie@stud.uni-saarland.de>
2020-08-01 13:32:33 +0200

    python scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -4,7 +4,7 @@ import time
 import os, shutil
 
 def job():
-    shutil.copyfile('skills/mycroft_bachelor_thesis.smilesmiley/setup_one/start.sh','./')
+    shutil.copy('skills/mycroft_bachelor_thesis.smilesmiley/setup_one/start.sh','./')
     subprocess.call('start.sh')
 
     # to do do only once the job
@@ -12,7 +12,7 @@ def job():
 # schedule.every(10).minutes.do(job)
 # schedule.every().hour.do(job)
 # schedule.every().day.at("10:30").do(job)
-schedule.every().day.at("12:30").do(job)
+schedule.every().day.at("12:34").do(job)
 
 while 1:
     schedule.run_pending()
