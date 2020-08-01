9a1e38337337cb84a8a9cb96311a4405856e2cfc
smilesmiley <s8aagrie@stud.uni-saarland.de>
2020-08-01 13:26:15 +0200

    cli deletion

--- scheduler.py
+++ scheduler.py
@@ -1,16 +1,18 @@
 import schedule
 import subprocess
 import time
+import os, shutil
 
 def job():
-    subprocess.call('start.sh >/tmp/pycommand.log 2>&1')
+    shutil.copyfile('skills/mycroft_bachelor_thesis.smilesmiley/setup_one/start.sh','./')
+    subprocess.call('start.sh')
 
     # to do do only once the job
     return schedule.CancelJob
 # schedule.every(10).minutes.do(job)
 # schedule.every().hour.do(job)
 # schedule.every().day.at("10:30").do(job)
-schedule.every().day.at("12:10").do(job)
+schedule.every().day.at("12:30").do(job)
 
 while 1:
     schedule.run_pending()
